f7b0f2bfe8350a369f64bbaa977595730c31abcb
Sue Feng Tan <suefengtan@gmail.com>
2019-10-26 12:02:02 -0700

    rough wireframe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,16 @@ function App() {
     <main className="app">
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-      <section class="section">
-
+      <section class="main-section">
+        main
       </section>
+      <aside class="sidebar">
+        sidebar
+      </aside>
+      <footer class="footer">
+        footer
+      </footer>
     </main>
   );
 }

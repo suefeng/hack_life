e910ca38f55e410029f42a03229fdbadeef94025
David Tan <dtan@instructure.com>
2019-10-26 19:39:03 -0700

    adding the build folder

---
+++ frontend/build/precache-manifest.e29c0ef97eb1bc7a36f73392ad2337b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ba3816f99dbbc9dad2a7a1cd960f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ff0e326d1543250c76b",
+    "url": "/static/css/main.c0567db2.chunk.css"
+  },
+  {
+    "revision": "faf58b1f2fd2108c1abd",
+    "url": "/static/js/2.9ca7be88.chunk.js"
+  },
+  {
+    "revision": "7ff0e326d1543250c76b",
+    "url": "/static/js/main.d1912e75.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);

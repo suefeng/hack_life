39a1655f1e5af5a501dd6686ef35ece60d687b33
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2019-10-27 08:04:58 -0700

    Use moment and list of practitioners

--- frontend/src/dashboardData/appointments.js
+++ frontend/src/dashboardData/appointments.js
@@ -1,28 +1,47 @@
+import { weekToDate } from '../constants'
+
+export const practitioners = [
+  { "name": "Emma Smith", "type": "Maternal-Fetal Medicine Specialist", "description": "ObGyn physician with expertise in high-risk pregnancies." },
+  { "name": "Daniel Jazz", "type": "Obstetrician and Gynecologist (ObGyn)", "description": "An ObGyn physician delivers babies and specializes in treating diseases of the female reproductive organs." },
+  { "name": "Lyle Burns", "type": "Registered Dietitian Nutritionist with specialty in medical nutrition therapy", "description": "An RDN is a food and nutrition expert. Medical nutrition therapy treats chronic conditions using a tailored nutrition plan." },
+]
+
 export const data = {
     "appointments": [
         {
-            "date": "2019-10-10",
-            "text": "Checkup: Dr. Smith",
+            "date": weekToDate(24),
+            "text": "Checkup",
+            "practitioner": practitioners[0],
             "time": "3:00-3:30pm"
         },
         {
-            "date": "2019-10-11",
-            "text": "Follow up: Dr. Jazz",
+            "date": weekToDate(24).add(1, 'days'),
+            "text": "Follow up",
+            "practitioner": practitioners[1],
             "time": "11:00-11:30pm"
         },
         {
-            "date": "2019-10-12",
-            "text": "Checkup: Dr. Smith",
+            "date": weekToDate(24).add(2, 'days'),
+            "text": "Checkup",
+            "practitioner": practitioners[0],
             "time": "3:00-3:30pm"
         },
         {
-            "date": "2019-11-14",
-            "text": "Follow up: Dr. Jazz",
+            "date": weekToDate(24).add(3, 'days'),
+            "text": "Follow up",
+            "practitioner": practitioners[1],
             "time": "11:00-11:30pm"
         },
         {
-            "date": "2019-12-10",
-            "text": "Checkup: Dr. Smith",
+            "date": weekToDate(25),
+            "text": "Checkup",
+            "practitioner": practitioners[0],
+            "time": "3:00-3:30pm"
+        },
+        {
+            "date": weekToDate(25),
+            "text": "Checkup",
+            "practitioner": practitioners[0],
             "time": "3:00-3:30pm"
         }
     ].map(({ date, text, time }) => ({

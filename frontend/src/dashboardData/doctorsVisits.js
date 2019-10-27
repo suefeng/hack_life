efd6762f0f7434dd91e5731f68a48ba613da81e2
Sue Feng Tan <suefengtan@gmail.com>
2019-10-27 09:58:59 -0700

    doctors portal and entries structures and styles

---
+++ frontend/src/dashboardData/doctorsVisits.js
@@ -0,0 +1,22 @@
+export const data = {
+    "doctorsVisits": [
+        {
+            "doctor": "Dr. Jazz",
+            "date": "2019-10-29",
+            "Purpose": "Referral visit",
+            "url": "#"
+        },
+        {
+            "doctor": "Dr. Smith",
+            "date": "2019-10-1",
+            "Purpose": "1st trimester check in",
+            "url": "#"
+        },
+        {
+            "doctor": "Dr. Jazz",
+            "date": "2019-9-1",
+            "Purpose": "1st trimester visit",
+            "url": "#"
+        }
+    ]
+};
